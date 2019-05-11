7296a3bef29932a754845c50d242494af521fa67
JadeSchack <scha8916@stthomas.edu>
2019-05-11 04:26:02 +0000

    Remotely store data via Google Sheets and Submit button

--- BballApp/Bball.r
+++ BballApp/Bball.r
@@ -1,10 +1,14 @@
 library(shiny)
 
+#fields we want to save
+fields <- c("date", "team", "player", "location", "hit", "result")
+
 ui <- fluidPage(
+  DT::dataTableOutput("responses", width = 300), tags$hr(),
   titlePanel("UST Men's Baseball"),
   sidebarLayout(
     sidebarPanel(
-      dateInput("var", 
+      dateInput("date", 
                 label = "Date:", 
                 value = NULL, 
                 min = NULL, 
@@ -17,7 +21,7 @@ ui <- fluidPage(
                 autoclose = TRUE,
                 datesdisabled = NULL, 
                 daysofweekdisabled = NULL),
-      selectInput("var", 
+      selectInput("team", 
                   label = "Team:",
                   choices = list("Augsburg University", 
                                  "Bethel University",
@@ -30,27 +34,27 @@ ui <- fluidPage(
                                  "St. Olaf College",
                                  "University of St. Thomas"),
                   selected = "University of St. Thomas"),
-      numericInput("var", 
+      numericInput("player", 
                    label = "Player Number:", 
                    value = 00, 
                    min = 00, 
                    max = 99, 
                    step = 1,
                    width = NULL),
-      numericInput("var", 
-                   label = "Location of Hit", 
+      numericInput("location", 
+                   label = "Location of Hit:", 
                    value = 00, 
                    min = 0, 
                    max = 12, 
                    step = 1,
                    width = NULL),
-      selectInput("var", 
+      selectInput("hit", 
                   label = "Type of Hit:",
                   choices = list("None",
                                  "Ground Ball",
                                  "Line Drive",
                                  "Pop Fly")),
-      selectInput("var", 
+      selectInput("result", 
                   label = "Batting Result:",
                   choices = list("Out",
                                  "Walk",
@@ -58,21 +62,52 @@ ui <- fluidPage(
                                  "Double",
                                  "Triple",
                                  "Home Run")),
-      img(src = "UST.logo_.c.jpg", height = 100, width = 235)
+      img(src = "UST.logo_.c.jpg", height = "100%", width = "100%")
     ),
     mainPanel(
-      tags$head(tags$script(src = "message-handler.js")),
-      actionButton(style ="display:inline-block;width:20%;text-align:center;","do", "Next", style="float:right"),
-      br(),
       img(src = "field.png", height = "75%", width = "75%"),
       br(),
-      tags$head(tags$script(src = "message-handler.js")),
-      actionButton(style ="display:inline-block;width:20%;text-align:center;","do", "End Game", style="float:right")
+      actionButton("submit", "Submit", style = "float:right")
     )
   )
 )
 
+library(googlesheets)
+suppressPackageStartupMessages(library(dplyr))
+
+table <- "responses"
+
+saveData <- function(data) {
+  # Grab the Google Sheet
+  sheet <- gs_title(table)
+  # Add the data as a new row
+  gs_add_row(sheet, input = data)
+}
+
+loadData <- function() {
+  # Grab the Google Sheet
+  sheet <- gs_title(table)
+  # Read the data
+  gs_read_csv(sheet)
+}
+
+
+
+
 server <- function(input, output) {
+  formData <- reactive({
+    data <- sapply(fields, function(x) input[[x]])
+    data
+  })
+  observeEvent(input$submit, {
+    saveData(formData())
+  })
+  # Show the previous responses
+  # (update with current response when Submit is clicked)
+  output$responses <- DT::renderDataTable({
+    input$submit
+    loadData()
+  })     
 }
 
 shinyApp(ui = ui, server = server)
